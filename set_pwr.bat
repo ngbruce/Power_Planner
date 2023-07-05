48be3e2805141c80835a3144cc69b774277b66ab
NgBruce <woobruce@163.com>
2023-07-05 20:16:34 +0800

    add chcp for bat

--- set_pwr.bat
+++ set_pwr.bat
@@ -1,5 +1,8 @@
 @echo off
 python main.py
+chcp 65001
 echo ======================
+echo -程序结束，按任意键进入git工具-
+pause
 call set_git.bat
 exit
--- set_pwr_env.bat
+++ set_pwr_env.bat
@@ -3,7 +3,9 @@ set "WINPYDIR=%~dp0\..\chatRecorder_Env"
 set "PATH=%WINPYDIR%\;%WINPYDIR%\DLLs;%WINPYDIR%\Scripts;%PATH%;"
 set "PYTHON=%WINPYDIR%\python.exe "
 PYTHON main.py
+chcp 65001
 echo ======================
+echo -程序结束，按任意键进入git工具-
 pause
 call set_git.bat
 exit
